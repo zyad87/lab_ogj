1838c7561860981340d216f4411237545976d130
Zyad Dev <117089930+zyad87@users.noreply.github.com>
2024-09-10 11:20:39 +0300

    Lab2

--- obj.js
+++ obj.js
@@ -37,3 +37,114 @@ let user = {
   console.log(user.skill[2]);
   
   console.log(user.skill.join(' , '));
+
+
+
+
+
+// ------------------------------- Lab2 -------------------------------
+
+characters = [
+    {
+      name: 'Luke Skywalker',
+      height: 177,
+      gender: 'male',
+      mass: 77,
+      eye_color: 'brown',
+    },
+    {
+      name: 'Leia Organa',
+      height: 160,
+      gender: 'female',
+      mass: 56,
+      eye_color: 'blue',
+    },
+    {
+      name: 'Han Solo',
+      height: 180,
+      gender: 'male',
+      mass: 80,
+      eye_color: 'brown',
+    },
+    {
+      name: 'Chewie',
+      height: 222,
+      gender: 'male',
+      mass: 190,
+      eye_color: 'black',
+    },
+    {
+      name: 'kevien',
+      height: 106,
+      gender: 'male',
+      mass: 32.2,
+      eye_color: 'red',
+    },
+  ];
+  
+  let blueEyeColor = characters.find(
+    (character) => character.eye_color === 'blue'
+  );
+  let firstName = blueEyeColor ? blueEyeColor.name.split(' ')[0] : null;
+  
+  console.log(firstName);
+  
+  let mass50 = characters.find((character) => {
+    if (character.mass > 50) {
+      return character;
+    }
+  });
+  
+  console.log(mass50.gender);
+  
+  let height200 = characters.filter((character) => character.height < 200);
+  
+  console.log(height200);
+  
+  let maleCharacter = characters.filter((character) => {
+    return character.gender === 'male';
+  });
+  
+  console.log("/n");
+  console.log(maleCharacter);
+  
+  let namesOnly = [];
+  
+  characters.forEach((e) => {
+    namesOnly.push(e.name);
+  });
+  console.log(namesOnly);
+  
+  let heightOnly = [];
+  
+  characters.forEach((e) => {
+    heightOnly.push(e.height);
+  });
+  
+  console.log("/n");
+  console.log(heightOnly);
+  
+  let sortedByMass = characters.sort((e1, e2) => {
+    return e1.mass - e2.mass;
+  });
+  console.log("/n");
+  console.log(sortedByMass);
+  
+  let sortedByHeight = characters.sort((e1, e2) => {
+    return e2.height - e1.height;
+  });
+  
+  console.log("/n");
+  console.log(sortedByHeight);
+  
+  console.log("/n");
+  console.log(characters.every((e) => e.mass > 45));
+  
+  console.log("/n");
+  console.log(characters.every((e) => e.height > 200));
+  
+  console.log("/n");
+  console.log(characters.some((e) => e.eye_color === 'blue'));
+  
+  console.log("/n");
+  console.log(characters.some((e) => e.height > 210));
